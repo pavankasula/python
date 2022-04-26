2031a5a69eef0764ed318a28eb350dada010b4eb
pavankasula <99252227+pavankasula@users.noreply.github.com>
2022-04-26 16:32:40 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,15 @@
+a=["orange","mango","grapes"]
+print(len(a))
+print(a)
+a.append("guava")
+print(a)
+a.insert(2,"strawberry")
+print(a)
+print(type(a))
+a.remove("grapes")
+print(a)
+del a[1]
+print(a)
+
+a.remove("orange")
+print(a)
